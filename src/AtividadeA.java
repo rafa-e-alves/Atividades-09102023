356018f31e3a8780cb0cf8b015b407a5d1d2e7d7
PedroSafero <120952332+PedroSafero@users.noreply.github.com>
2023-11-01 18:14:32 -0300

    Initial commit

---
+++ src/AtividadeA.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class AtividadeA {
+    public static void main(String[] args) {
+        Scanner entry = new Scanner(System.in);
+
+        int numero;
+
+        System.out.print("Informe um número inteiro positivo: ");
+        numero = entry.nextInt();
+
+        if (numero < 0) {
+            System.out.println("O número deve ser inteiro positivo.");
+        } else if (numero == 0) {
+            System.out.println("0 é nulo, informe um número inteiro positivo");
+        } else {
+            while (numero > 0) {
+                System.out.println(numero);
+                numero--;
+            }
+            System.out.println("Limite atingido: Zero!");
+        }
+    }
+}
