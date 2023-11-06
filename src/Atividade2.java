384c6c9b669b2be8bbf212dc358b758e6963cb98
PedroSafero <120952332+PedroSafero@users.noreply.github.com>
2023-11-06 14:34:00 -0300

    Second commit

---
+++ src/Atividade2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Atividade2 {
+    public static void main(String[] args) {
+        Scanner entry = new Scanner(System.in);
+
+        int tamanho;
+
+        System.out.println("Digite o tamanho do quadrado, somente de 1 a 10:");
+        tamanho = entry.nextInt();
+
+        if (tamanho >= 1 && tamanho <= 10) {
+            System.out.println("Letra A:");
+            for (int i = 0; i < tamanho; i++) {
+                for (int j = 0; j < tamanho; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+
+            System.out.println("\nLetra B:");
+            for (int i = 0; i < tamanho; i++) {
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+
+            System.out.println("\nLetra C:");
+            for (int i = 0; i < tamanho; i++) {
+                for (int j = 0; j < tamanho - i; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("Tamanho maior que o limite (utilize o tamanho de 1 a 10).");
+        }
+    }
+}
